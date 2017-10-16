e5ccc694fd29e43c94463a93bf5fab183f3139c8
WGC <450199754@qq.com>
2017-10-16 09:41:32 +0800

    Initial commit

---
+++ 01-01-DUCK/src/Duck.java
@@ -0,0 +1,23 @@
+/**
+ * Created by 45019 on 2017/10/15.
+ */
+public abstract class Duck {
+
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public abstract void display();
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void swim() {
+        System.out.println("duck swim");
+    }
+
+}
---
+++ 01-01-DUCK/src/FlyBehavior.java
@@ -0,0 +1,6 @@
+/**
+ * Created by 45019 on 2017/10/15.
+ */
+public interface FlyBehavior {
+    public void fly();
+}
